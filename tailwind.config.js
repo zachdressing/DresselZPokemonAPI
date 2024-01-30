ddf1533869dc068d08ac06c238a85fb67f4d5ba7
zachdressing <zed_dressel@hotmail.com>
2024-01-30 07:00:33 -0800

    ini

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bgImg' : "url('/assets/pokemartLogo.png')"
+      }
+    },
+  },
+  plugins: [],
+}
